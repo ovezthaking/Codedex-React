01cb29a49b99300e53f6df0c952a96dbc609ff7f
ovezthaking <oliwerx12@gmail.com>
2025-08-26 03:19:24 +0200

    Haiku done

---
+++ Haiku/haiku/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+import React from "react";
+import Haiku from "./Haiku";
+
+export default function App() {
+  return <Haiku />;
+}
---
+++ Haiku/haiku/src/Haiku.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function Haiku() {
+  const [theme, setTheme] = useState("");
+
+  function handleSelectChange(e) {
+    setTheme(e.target.value);
+  }
+
+  const themes = {
+    nature: [
+      "A gentle breeze blows",
+      "Leaves rustle in the soft wind",
+      "Nature whispers peace",
+    ],
+    love: [
+      "Heartbeats softly sound",
+      "Love's sweet melody resounds",
+      "Together we're found",
+    ],
+    seasons: [
+      "Spring blooms with new life",
+      "Summer's warmth and joy arrive",
+      "Autumn leaves fall, sigh",
+    ],
+  };
+
+  function generateHaiku() {
+    if (theme) {
+      return themes[theme].map(function (line, index) {
+        return <p key={index}>{line}</p>;
+      });
+    }
+  }
+
+  return (
+    <div className="container">
+      <h2>Haiku Builder</h2>
+      <select onChange={handleSelectChange}>
+        <option value=''>--Select a theme--</option>
+        <option value='nature'> 🌲 Nature</option>
+        <option value='love'>❤️ Love</option>
+        <option value='seasons'>🍂 Seasons</option>
+      </select>
+      <div>{generateHaiku()}</div>
+      <br></br>
+      <textarea className='haiku-textarea' placeholder='Write your own haiku here...'></textarea>
+    </div>
+  );
+}
