69b75884e726a2417e0358c500122ebf7cf95b1d
ovezthaking <oliwerx12@gmail.com>
2025-10-15 13:32:40 +0200

    Book Finder finished

---
+++ Book Finder/book-finder/src/BookFinder.js
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+
+export default function BookFinder() {
+    const [query, setQuery] = useState("");
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+  async function fetchData(filterValue = "") {
+    setError(null);
+
+    try {
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error('Data fetching failed!');
+        }
+      const data = await response.json();
+      console.log(data.items);
+      let filteredBooks = data.items;
+      if (filterValue !== "") {
+        filteredBooks = filteredBooks.filter(function (book) {
+          return (
+            book.volumeInfo &&
+            book.volumeInfo.categories &&
+            book.volumeInfo.categories.includes(filterValue)
+          );
+        });
+      }
+
+      setBooks(filteredBooks);
+      setLoading(false);
+    } catch (error) {
+      setError("Error fetching data. Please try again later.");
+    }
+  }
+
+  useEffect(
+    function () {
+      if (query !== "") {
+        fetchData();
+      }
+    },
+    [loading],
+  );
+
+  function handleInputChange(event) {
+    setQuery(event.target.value);
+  }
+
+  function handleSearch(event) {
+    setLoading(true);
+    fetchData();
+  }
+
+  function handleFilterChange(event) {
+    setLoading(true);
+    fetchData(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>Book Finder</h1>
+      <div id="input-fields">
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Search for books..."
+        />
+        <button type="submit" onClick={handleSearch}>
+          Search
+        </button>
+        <br />
+        <label htmlFor="filter">Filter by:</label>
+        <select id="filter" onChange={handleFilterChange}>
+          <option value="">All</option>
+          <option value="Paid ebooks">Paid E-books</option>
+          <option value="free-ebooks">Free E-books</option>
+        </select>
+      </div>
+      {loading && <p>loading...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {books.map(function (book) {
+          return (
+            <li key={book.id} className="book">
+              {book.volumeInfo.imageLinks &&
+                book.volumeInfo.imageLinks.thumbnail && (
+                  <img
+                    src={book.volumeInfo.imageLinks.thumbnail}
+                    alt={book.volumeInfo.title}
+                    className="book-cover"
+                  />
+                )}
+              <div className="book-info">
+                <h2>{book.volumeInfo.title}</h2>
+                <p>
+                  {book.volumeInfo.authors
+                    ? book.volumeInfo.authors.join(", ")
+                    : "Unknown Author"}
+                </p>
+
+                <p>
+                  {book.volumeInfo.description
+                    ? `${book.volumeInfo.description.substring(0, 100)}...`
+                    : "No description available"}
+                </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
