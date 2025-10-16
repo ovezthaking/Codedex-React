1aa05c0a974b6642393016edcd67c93c9ff70bc3
ovezthaking <oliwerx12@gmail.com>
2025-10-16 02:27:50 +0200

    paper route

---
+++ Routing/Paper Route/paper-route/src/App.js
@@ -0,0 +1,14 @@
+// Add imports here 💖
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <div>
+      <h1>Map Navigation</h1>
+      <Map />
+      {/* Add Routes here 💖 */}
+    </div>
+  );
+}
+
+export default App;
